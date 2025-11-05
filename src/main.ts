030350bd68deb31a35bbe2eaa0875e7bf9586778
Richard Gross <richard.gross@maibornwolff.de>
2025-11-05 11:48:15 +0100

    ^ d add startup-docs for how to use the mcp

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,14 @@ function main() {
     const app = configureHttpApp(mcpHandler)
     app.listen(port, () => {
         console.log(`MCP Server running on http://localhost:${port}/mcp`);
+        console.log(`Claude Code specifics:`);
+        console.log(`* Add to claude code via: claude mcp add --transport http richargh http://localhost:${port}/mcp`);
+        console.log(`* Check status in claude: /mcp`);
+        console.log(`* Remove mcp once you have tried it: claude mcp remove richargh`);
+        console.log(`* Try also some of the actions:`);
+        console.log(`** Use the prompt by calling slash command /grade-generated-poem`);
+        console.log(`** Ask it to 'Multiply 5 and 7'`);
+
     }).on('error', error => {
         console.error('Server error:', error);
         process.exit(1);
